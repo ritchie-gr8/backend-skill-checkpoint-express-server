13641f80292f6ec4bdfa8c8912be0038a31aac2f
ritchie-gr8 <rit.yotsathon@gmail.com>
2025-04-11 09:53:58 +0700

    feat: add docker compose for db and change test endpoint to health check

--- app.mjs
+++ app.mjs
@@ -1,13 +1,12 @@
 import express from "express";
+import { checkHealth } from "./controllers/health.mjs";
 
 const app = express();
 const port = 4000;
 
 app.use(express.json());
 
-app.get("/test", (req, res) => {
-  return res.json("Server API is working 🚀");
-});
+app.get("/health", checkHealth);
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
---
+++ controllers/health.mjs
@@ -0,0 +1,18 @@
+import connectionPool from "../utils/db.mjs";
+
+export const checkHealth = async (req, res) => {
+  try {
+    await connectionPool.query("SELECT 1");
+    return res.status(200).json({
+      status: "OK 👌",
+      db: "Connected ✅",
+      message: "Server API is working 🚀",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "ERROR",
+      db: "Disconnected ❌",
+      message: "Server API is working, but DB connection failed ❌",
+    });
+  }
+};
