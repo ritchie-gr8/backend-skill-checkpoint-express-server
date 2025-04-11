84941c6d2c114f507d0bd99be524aa730e807961
ritchie-gr8 <rit.yotsathon@gmail.com>
2025-04-11 12:46:30 +0700

    feat: add answers middleware to validate request and setup repo/route files for answers endpoints

---
+++ middlewares/answers.mjs
@@ -0,0 +1,45 @@
+import questionRepo from "../repositories/questions.mjs";
+import { HTTP_STATUS } from "../utils/httpStatus.mjs";
+import { errorResponse } from "../utils/responseUtils.mjs";
+
+export const validateAnswer = (req, res, next) => {
+  try {
+    const { content } = req.body;
+
+    if (!content) {
+      throw new Error("Invalid request data.");
+    }
+
+    next();
+  } catch (error) {
+    return errorResponse({
+      res,
+      message: error.message,
+      status: HTTP_STATUS.BAD_REQUEST,
+    });
+  }
+};
+
+export const validateQuestionExists = async (req, res, next) => {
+  try {
+    const { questionId } = req.params;
+
+    const question = await questionRepo.findById(questionId);
+    if (!question) {
+      return errorResponse({
+        res,
+        message: "Question not found",
+        status: HTTP_STATUS.NOT_FOUND,
+      });
+    }
+
+    req.question = question;
+    next();
+  } catch (error) {
+    return errorResponse({
+      res,
+      message: "Unable to fetch question.",
+      status: HTTP_STATUS.INTERNAL_SERVER_ERROR,
+    });
+  }
+};
