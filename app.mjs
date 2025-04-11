29ab16f9e24903c1ff5c05c071576db0cd8d5e7b
ritchie-gr8 <rit.yotsathon@gmail.com>
2025-04-11 17:21:28 +0700

    feat: add API doc using Swagger UI and add readme content

--- app.mjs
+++ app.mjs
@@ -4,6 +4,8 @@ import questionsRouter from "./routes/questions.mjs";
 import answersRouter from "./routes/answers.mjs";
 import voteRouter from "./routes/vote.mjs";
 import { validateAnswerExists } from "./middlewares/answers.mjs";
+import YAML from "yamljs";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 const port = 4000;
@@ -11,6 +13,9 @@ const port = 4000;
 app.use(express.json());
 
 app.get("/health", checkHealth);
+
+const swaggerDocument = YAML.load("./docs/swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/questions", questionsRouter);
 app.use("/questions/:questionId/answers", answersRouter);
